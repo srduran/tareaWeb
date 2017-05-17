b7fb74a9f57d93c01efa1a9d1bb6f5c1b3646347
srduran <srduran@miuandes.cl>
2017-05-17 09:54:33 -0400

    arreglos para devise

--- tarea1/app/controllers/application_controller.rb
+++ tarea1/app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_person!
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :email])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :email])
+  end
   protect_from_forgery with: :exception
   before_action :authenticate_person!
 end
--- tarea1/config/routes.rb
+++ tarea1/config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :documents
   resources :categories
   resources :people
+  root to: 'people#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
