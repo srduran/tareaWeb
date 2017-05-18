482c3f9c7875654bc40cc666656e9704a607a7ef
José Miguel Alvarez <jmalvarez@miuandes.cl>
2017-05-18 06:02:36 -0400

    boostrap and markdown details

--- tarea1/config/routes.rb
+++ tarea1/config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   resources :documents
   resources :categories
   resources :people
-  root to: 'people#index'
+  root to: 'documents#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
