7c9a664fa27da21639d1b0315a8bdef67f4a3d48
Jaime Echeverría <rpggrenade@gmail.com>
2017-05-28 02:19:40 -0400

    Bootstrap parcialmente integrado, Barra de búsqueda integrada.

--- tarea1/app/models/document.rb
+++ tarea1/app/models/document.rb
@@ -3,6 +3,10 @@ class Document < ApplicationRecord
   validates :title, {presence: true, length: {in: 10..50}, uniqueness: true}
   validates :text, {presence: true, length: {minimum: 10}}
 
+  def self.search(search)
+    where("title LIKE ?", "%#{search}%")
+  end
+
   def to_s
     return title
   end
