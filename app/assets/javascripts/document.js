f301c433abe69467fbaf30ff9f0786f6a6d656ca
Jaime Echeverría <rpggrenade@gmail.com>
2017-06-17 18:50:22 -0400

    Heart Color Change.

--- app/assets/javascripts/document.js
+++ app/assets/javascripts/document.js
@@ -1,7 +1,6 @@
 /**
  * Created by Jaime on 6/1/2017.
  */
-
 var converter = new Markdown.Converter();
 function convert() {
     var area = document.getElementById('text-doc');
@@ -10,7 +9,14 @@ function convert() {
 }
 
 $(document).ready(function (){
+
     $("#text-doc").keyup(function () {
         convert()
-    })
+    });
+    //Include code relating to reading whether the user liked this document
+    //need to add a table for "likes" of some kind.
+    $('#like-button').click(function () {
+        $(this).toggleClass("active");
+        $(this).blur();
+    });
 });
