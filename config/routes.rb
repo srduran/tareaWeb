103198fa67696652753c90cc31298c2b03fa565c
srduran <srduran@miuandes.cl>
2017-06-17 13:24:59 -0400

    little changes for sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
 Rails.application.routes.draw do
-  resources :authors
-  resources :enrollments
-  devise_for :people
+  devise_scope :person do
+    get "/sign_in" => "devise/sessions#new" # custom path to login/sign_in
+    get "/sign_up" => "devise/registrations#new", as: "new_person_registration" # custom path to sign_up/registration
+  end
+  devise_for :people, :skip => [:registrations]
+  as :person do
+    get 'people/edit' => 'devise/registrations#edit', :as => 'edit_person_registration'
+    put 'people/' => 'devise/registrations#update', :as => 'person_registration'
+  end
   resources :documents do
     get 'my_documents', to: 'documents#my_documents', on: :member
   end
+  resources :authors
+  resources :enrollments
   resources :categories
   resources :people
   root to: 'documents#index'
